819ff8557d64906346ba01683c19467b3f217320
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-03-13 00:52:03 -0300

    Adicionando Funcoes as Opcoes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+    print("""
 
 ░█████╗░██╗░░██╗███████╗███████╗  ░█████╗░██████╗░██╗░█████╗░████████╗██╗██╗░░░██╗░█████╗░
 ██╔══██╗██║░░██║██╔════╝██╔════╝  ██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝██║██║░░░██║██╔══██╗
@@ -8,24 +11,59 @@ print("""
 ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░  ░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░░░░╚════╝░"
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listagem restaurante')
-print('3. Ativar restaurante') # Não aparece no app direto
-print('4. Sair\n')
+def exibir_opções():
+    print('1. Cadastrar restaurante')
+    print('2. Listagem restaurante')
+    print('3. Ativar restaurante') # Não aparece no app direto
+    print('4. Sair\n')
+
+# Bloco de codigo para finalizar o App
+def Finalizar_app(): 
+    os.system('cls')
+    print('Finalizando o app\n')
+    print()
+
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-# opcao_escolhida = int(opcao_escolhida)
+# Opção 1
+def cadastrar_novo_restaurante():
+    pass
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes')
-    # Todo cod de cadastro do restaurante
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    print('Encerrando o programa')
+
+
+
+def escolher_opção():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            Finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+# Ordem de Saida
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opções()
+    escolher_opção()
+
+if __name__ == '__main__':
+    main()
     
 
 
