dec1c43edde2fa44a52c3e6d5626a0659bd70065
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-03-13 10:18:12 -0300

    Mais adicionais

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
-restaurantes = [{'Nome':'Divino', 'Categoria':'Brasileira', 'Ativo':False}, 
-                {'Nome':'Planet Pizzas', 'Categoria':'Italiana','Ativo': True},
-                {'Nome':'Mak Burger', 'Categoria':'Hamburgueria', 'Ativo':False}]
+restaurantes = [{'Nome': 'Divino', 'Categoria': 'Brasileira', 'Ativo': False},
+                {'Nome': 'Planet Pizzas', 'Categoria': 'Italiana', 'Ativo': True},
+                {'Nome': 'Mak Burger', 'Categoria': 'Hamburgueria', 'Ativo': False}]
+
 
 def exibir_nome_do_programa():
     print("""
@@ -15,39 +16,48 @@ def exibir_nome_do_programa():
 ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░  ░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░░░░╚════╝░"
 """)
 
+
 def exibir_opções():
     print('1. Cadastrar restaurante')
     print('2. Listagem restaurante')
-    print('3. Ativar restaurante') # Não aparece no app direto
+    print('3. Ativar restaurante')  # Não aparece no app direto
     print('4. Sair\n')
 
 # Bloco de codigo para finalizar o App
-def Finalizar_app(): 
+
+
+def Finalizar_app():
     exibir_subtitulo('Finalizar app')
+
 
 def voltar_ao_menu_principal():
     input('\nDigite uma tecla para voltar ao menu principal')
     main()
 
+
 def opcao_invalida():
     print('Opção inválida!\n')
     voltar_ao_menu_principal()
 
+
 def exibir_subtitulo(texto):
-     os.system('cls')
-     print(texto)
-     print()
+    os.system('cls')
+    print(texto)
+    print()
 
 
 # Opção 1 - Cadasto
 def cadastrar_novo_restaurante():
-   exibir_subtitulo('Cadastro de novos restaurantes')
-   nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-   categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
-   dados_do_restaurante = {'Nome': nome_do_restaurante, 'Categoria': categoria, 'Ativo':False}
-   restaurantes.append(dados_do_restaurante)
-   print(f'O resaurante {nome_do_restaurante} foi cadastrado com êxito!')
-   voltar_ao_menu_principal()
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input(
+        'Digite o nome do restaurante que deseja cadastrar: ')
+    categoria = input(
+        f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'Nome': nome_do_restaurante,
+                            'Categoria': categoria, 'Ativo': False}
+    restaurantes.append(dados_do_restaurante)
+    print(f'O resaurante {nome_do_restaurante} foi cadastrado com êxito!')
+    voltar_ao_menu_principal()
 
 
 # Opção 2 - Lista
@@ -61,11 +71,25 @@ def listar_restaurantes():
         print(f'- {nome_restaurante} | {categoria} | {ativo}')
     voltar_ao_menu_principal()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['Nome']:
+            restaurante_encontado = True
+            restaurante['Ativo'] = not restaurante['Ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante ['Ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+            
+    if not restaurante_encontado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 
 # Opção 3 - Ativar
-
-
 
 
 def escolher_opção():
@@ -73,13 +97,12 @@ def escolher_opção():
         opcao_escolhida = int(input('Escolha uma opção: '))
         # opcao_escolhida = int(opcao_escolhida)
 
-
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             Finalizar_app()
         else:
@@ -88,14 +111,14 @@ def escolher_opção():
         opcao_invalida()
 
 # Ordem de Saida
+
+
 def main():
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opções()
     escolher_opção()
 
+
 if __name__ == '__main__':
     main()
-    
-
-
